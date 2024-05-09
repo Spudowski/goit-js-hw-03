51bd97cb7f93cd8a1df9a71d211e0dbf86c1634a
Spudowski <156944179+Spudowski@users.noreply.github.com>
2024-05-09 15:22:26 +0200

    Add files via upload

---
+++ task-addons.js
@@ -0,0 +1,11 @@
+const flowers = ["rose", "dandelion", "orchid"];
+    console.log(flowers, flowers.length);
+
+    flowers[flowers.length] = "poppy";
+    console.log(flowers, flowers.length);
+
+    flowers[flowers.length] = "lilly";
+    console.log(flowers, flowers.length);
+
+    flowers[flowers.length] = "daisy";
+    console.log(flowers, flowers.length);
